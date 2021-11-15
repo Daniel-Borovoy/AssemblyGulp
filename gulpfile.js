caa836dc4d5cccef5c1537e62215ce9c78c15430
Daniil_Borovoy <74528634+daniil-borovoy@users.noreply.github.com>
2021-11-15 19:45:25 +0300

    refactoring gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,141 +1,145 @@
-const fileinclude = require("gulp-file-include");
+const project_folder = require('path').basename(__dirname)
+const source_folder = '#src'
+const PORT = 3000
 
-let project_folder = require("path").basename(__dirname);
-let source_folder = "#src";
-
-let path = {
-    build:{
-        html: project_folder + "/",
-        css: project_folder + "/css/",
-        js: project_folder + "/js/",
-        img: project_folder + "/img/",
-        fonts: project_folder + "/fonts/",
-    },
-    src:{
-        html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
-        css: source_folder + "/scss/style.scss",
-        js: source_folder + "/js/script.js",
-        img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
-        fonts: source_folder + "/fonts/*.ttf",
-    },
-    watch:{
-        html: source_folder + "/**/*.html",
-        css: source_folder + "/scss/**/*.scss",
-        js: source_folder + "/js/**/*.js",
-        img: source_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
-    },
-    clean: "./" + project_folder + "/"
-
+// Paths
+const path = {
+  build: {
+    html: project_folder + '/',
+    css: project_folder + '/css/',
+    js: project_folder + '/js/',
+    img: project_folder + '/img/',
+    fonts: project_folder + '/fonts/',
+  },
+  src: {
+    html: [source_folder + '/*.html', '!' + source_folder + '/_*.html'],
+    css: source_folder + '/scss/style.scss',
+    js: source_folder + '/js/*.js',
+    img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+    fonts: source_folder + '/fonts/*.ttf',
+  },
+  watch: {
+    html: source_folder + '/**/*.html',
+    css: source_folder + '/scss/**/*.scss',
+    js: source_folder + '/js/**/*.js',
+    img: source_folder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
+  },
+  clean: './' + project_folder + '/',
 }
 
-let { src, dest } = require("gulp"),
-    gulp = require("gulp"),
-    browsersync = require("browser-sync").create(),
-    fileInclude = require("gulp-file-include"),
-    del = require("del"),
-    scss = require("gulp-sass"),
-    autoprefixer = require("gulp-autoprefixer"),
-    group_media = require("gulp-group-css-media-queries"),
-    clean_css = require("gulp-clean-css"),
-    rename = require("gulp-rename"),
-    uglify = require("gulp-uglify-es").default,
-    imagemin = require("gulp-imagemin"),
-    webp = require("gulp-webp"),
-    webphtml = require("gulp-webp-html");
+// Plugin imports
+const { src, dest, series, parallel, watch } = require('gulp'),
+  browsersync = require('browser-sync').create(),
+  fileInclude = require('gulp-file-include'),
+  del = require('del'),
+  scss = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  group_media = require('gulp-group-css-media-queries'),
+  clean_css = require('gulp-clean-css'),
+  rename = require('gulp-rename'),
+  uglifyes = require('gulp-uglify-es').default,
+  imagemin = require('gulp-imagemin'),
+  webp = require('gulp-webp'),
+  webphtml = require('gulp-webp-html')
 
+const browserSync = () => {
+  browsersync.init({
+    server: {
+      baseDir: './' + project_folder + '/',
+    },
+    port: PORT,
+    notify: false,
+  })
+}
 
-function browserSync(params) {
-    browsersync.init({
-        server:{
-            baseDir: "./" + project_folder + "/"
+const html = () =>
+  src(path.src.html)
+    .pipe(fileInclude())
+    .pipe(webphtml())
+    .pipe(dest(path.build.html))
+    .pipe(browsersync.stream())
+
+const clean = () => del(path.clean)
+
+const css = () =>
+  src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: 'expanded',
+      })
+    )
+    .pipe(group_media())
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 5 versions'],
+        cascade: true,
+      })
+    )
+    // Uploading a file (not compressed)
+    .pipe(dest(path.build.css))
+    // Compress and upload the second css (already compressed)
+    .pipe(clean_css())
+    .pipe(
+      rename({
+        extname: '.min.css',
+      })
+    )
+    .pipe(dest(path.build.css))
+    .pipe(browsersync.stream())
+
+const js = () =>
+  src(path.src.js)
+    .pipe(fileInclude())
+    .pipe(dest(path.build.js))
+    .pipe(
+      uglifyes({
+        sourceMap: {
+          filename: 'script.js',
         },
-        port: 3000,
-        notify: false
-    })
+      })
+    )
+    .pipe(
+      rename({
+        extname: '.min.js',
+      })
+    )
+    .pipe(dest(path.build.js))
+    .pipe(browsersync.stream())
+
+const images = () =>
+  src(path.src.img)
+    .pipe(
+      webp({
+        quality: 70,
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(src(path.src.img))
+    .pipe(
+      imagemin({
+        progressive: true,
+        svgoPlugins: [{ removeViewBox: false }],
+        interlaced: true,
+        optimizationLevel: 3, // from 0 to 7
+      })
+    )
+    .pipe(dest(path.build.img))
+    .pipe(browsersync.stream())
+
+const watchFiles = () => {
+  watch([path.watch.html], html)
+  watch([path.watch.css], css)
+  watch([path.watch.js], js)
+  watch([path.watch.img], images)
 }
 
-function html() {
-    return src(path.src.html)
-        .pipe(fileInclude())
-        .pipe(webphtml())
-        .pipe(dest(path.build.html))
-        .pipe(browsersync.stream())
-}
+const build = series(clean, parallel(js, css, html, images))
+const watchMyFiles = parallel(build, watchFiles, browserSync)
 
-function clean() {
-    return del(path.clean);
-}
-
-
-function css() {
-    return src(path.src.css)
-        .pipe(
-            scss({
-                outputStyle: "expanded"
-            })
-        )
-        .pipe(group_media())
-        .pipe(
-            autoprefixer({
-                overrideBrowserslist: ["last 5 versions"],
-                cascade: true
-            })
-        )
-        // .pipe(webpcss())
-        // Выгружаем файл (не сжатый)
-        .pipe(dest(path.build.css))
-        // Сжимаем и выгружаем второй сжатый css
-        .pipe(clean_css())    
-        .pipe(rename({
-            extname: ".min.css"
-        }))
-        .pipe(dest(path.build.css))
-        .pipe(browsersync.stream())
-}
-
-function js() {
-    return src(path.src.js)
-        .pipe(fileInclude())
-        .pipe(dest(path.build.js))
-        .pipe(uglify())
-        .pipe(rename({
-            extname: ".min.js"
-        }))
-        .pipe(dest(path.build.js))
-        .pipe(browsersync.stream())
-}
-
-function images() {
-    return src(path.src.img)
-        .pipe(webp({
-            quality: 70
-        }))
-        .pipe(dest(path.build.img))
-        .pipe(src(path.src.img))
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{ removeViewBox: false}],
-            interlaced: true,
-            optimizationLevel: 3 // От 0 до 7
-        }))
-        .pipe(dest(path.build.img))
-        .pipe(browsersync.stream())
-}
-
-function watchFiles(params){
-    gulp.watch([path.watch.html],html);
-    gulp.watch([path.watch.css],css);
-    gulp.watch([path.watch.js],js);
-    gulp.watch([path.watch.img],images);
-}
-
-let build = gulp.series(clean, gulp.parallel(js, css, html, images));
-let watch = gulp.parallel(build, watchFiles, browserSync);
-
-exports.images = images;
-exports.js = js;
-exports.css = css;
-exports.html = html;
-exports.build = build;
-exports.watch = watch;
-exports.default = watch;
+exports.images = images
+exports.js = js
+exports.css = css
+exports.html = html
+exports.build = build
+exports.watchMyFiles = watchMyFiles
+exports.default = watchMyFiles
